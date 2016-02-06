5ec426e48dbc4ccbcac0491182c920b051cc4392
Michael Korzon <mclzor@gmail.com>
2016-02-05 18:06:07 -0600

    Update hw1.m

--- hw1.m
+++ hw1.m
@@ -1,32 +1,38 @@
 #!/usr/bin/octave
 
 %% Part 1
+
+% 1-a
 a = 1/15+sind(45)-4*pi;
+
+% 1-b
 b = sqrt(log(9)/log(3) + exp(1)^(9*i));
+
+% 1-c
 c = (45+7/5)/(factorial(3)+6*pi);
 
 fprintf('a = %f\n', a);
 fprintf('b = %f\n', b);
 fprintf('c = %f\n', c);
 
-%% Part 2
+%% Section 2
 m = input('Enter a mass in kg: ');
 v = input('Enter a velocity in m/s: ');
 c = 3.0e8;
 
-% a
+% 2-a
 kr = m*c^2*(1.0/sqrt(1-v^2/c^2)-1);
 fprintf('Relativistic Kinetic Energy: %.6e\n', kr);
 
-% b
+% 2-b
 kc = 1/2*m*v^2;
 fprintf('Classical Kinetic Energy: %.6e\n', kc);
 
-% c
+% 2-c
 d = abs(kc-kr);
 fprintf('Absolute Difference: %.6e\n', d);
 
-% d
+% 2-d
 v = c-1;
 kr = m*c^2*(1.0/sqrt(1-v^2/c^2)-1);
 kc = 1/2*m*v^2;
@@ -48,5 +54,18 @@ fprintf('\n');
 kmph = [0 10 20 30 40 50 60 70 80 90 100];
 mps = 1000/3600*kmph;
 conversion_table = [kmph; mps];
-fprintf('km/h   m/h\n');
-fprintf('%5.2f %5.2f\n', conversion_table);
+fprintf('km/h    m/h\n');
+fprintf('%6.2f %6.2f\n', conversion_table);
+fprintf('\n');
+
+%% Part 4
+n = input('Enter a number: ');
+mat = diag([1:n].^2) - diag(ones(1, n-1), 1) + diag(repmat(exp(1), 1, n-1).^[1:n-1], -1);
+disp(mat);
+fprintf('\n');
+
+%% Part 5
+mat = rand(3)
+s = sum(transpose(mat))
+mat = rand(3) ./ transpose(repmat(s, length(mat), 1));
+disp(mat)
