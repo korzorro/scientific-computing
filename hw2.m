7ec8eb49fcc0db70f160efb9e4e87af3c95f4f34
Michael Korzon <mclzor@gmail.com>
2016-02-19 22:27:18 -0600

    Finish homework 2

--- hw2.m
+++ hw2.m
@@ -1,16 +1,21 @@
 % Michael Korzon
-% 70-315 Scientific Computing
-% Dr. Szczurek
-% Due: 2/19/16
+% Scientific Computing 70-315
+% Spring 2016
+% Homework 2
+
 
 %% Problem 1
+
+e = exp(1);
+
 car1 = [12.4 12.6 14.0 12.2 12.5];
 car2 = [12.1 12.0 12.1 12.0 12.9];
 car3 = [12.5 11.9 12.4 15.1 11.8];
 cars = [car1; car2; car3]';
-num_cars = 5;
-car_nums = 1:3;
-% 1-a
+num_cars = 5; % Number of cars in each car vector 
+car_nums = 1:3; % Number of different types of cars
+
+% a
 means = mean(cars);
 medians = median(cars);
 stds = std(cars);
@@ -28,48 +33,56 @@ fprintf('\n');
 fprintf('The maximum for car %d is %.1f.\n', [car_nums; maxs]);
 fprintf('\n');
 
-% 1-b
+% b
 sem = stds ./ sqrt(num_cars);
 t_score = tinv([.025 .975], num_cars-1);
 ci = means + t_score' .* sem;
 
-fprintf('The 95%% confidence interval for car %d is %.2f to %.2f.\n', [car_nums; ci])
-fprintf('\n')
+fprintf('The 95%% confidence interval for car %d is %.2f to %.2f.\n', [car_nums; ci]);
+fprintf('\n');
 
-% 1-c
-fprintf('Car 3 should be considered the fastest because it has the highest value in\n')
-fprintf('the 95%% confidence interval. The car is also tied for highest mean speed\n')
-fprintf('and produced the single fastest test speed.\n')
+% c
+fprintf('Car 2 should be considered the fastest because it has the lowest median \n');
+fprintf('value and mean value. These are both good indicators of how fast the car is,\n');
+fprintf('but this does not give the full picture. The standard deviation of car 2 is\n');
+fprintf('relatively small, which means that the car has a narrow %%95 confidence \n');
+fprintf('interval, giving more significance to its low mean value. While car 3\n');
+fprintf('produced the two fastest results, it does not reliably outperform car 2\n\n');
 
 %% Problem 2
 
-%{
-(3 points) The population growth over time, P(t), can be modeled by the following function:
-P(t) = L/(1 + C*e^(kt))
-Use this equation to make 3 plots:
-• Plot 1 should show the population over time for t=0..100 with L=100, k=0.1, and 3 values of C (100,
-25, and 1).
-• Plot 2 should show the population over time for t=0..100 with L=100, C=25, and 3 values of k (0.1, 0.3,
-and 0.7).
-• Plot 3 should show the population over time for t=0..100 with C=25, k=0.1, and 3 values of L (100,
-200, and 400).
-HINT: define P(t) as a parameterized, anonymous function.
+e = exp(1);
 
-						  
-3. (3 points) Generate a 2 × 3 cell array with the following elements and output the contents to the screen:
-(1,1): a uniform random matrix of size 5.
-(2,1): the string "Hilbert".
-(1,2):
-(2,2): the function handle for the function sin
-(1,3): this is the square of the matrix in (1, 1) .
-(2,3): the logical value true.
-Additional Requirements
-• Name the m-file as hw2.m
-• At the top of the file, put a comment with your name, course name number, semester, and homework
-number.
-• Make sure to document your code using comments, including specifying the inputs and outputs (and
-associated units).
-• Put %% (cell separators) to separate each problem
-What to Turn In
-You will turn in the single hw2.m file using BlackBoard. 
-}%
+ti = [0 100]; % t interval
+_p = @(t, L, C, k) L ./ (1 + C.*e.^(-k.*t));
+figure;
+
+% plot 1
+% Paramterized functions can take in vectors for variables if defined correctly
+p = @(t) _p(t, 100, [100 25 1], .1);
+subplot(2, 2, 1);
+fplot(p, ti);
+title('Plot 1: 100 / (1 + C*e^{-.1*t}) C = [100 25 1]');
+
+% plot 2
+p = @(t) _p(t, 100, 25, [.1 .3 .7]);
+subplot(2, 2, 2);
+fplot(p, ti);
+title('Plot 2: 100 / (1 + 25*e^{-k*t}) k = [.1 .3 .7]');
+
+% plot 3
+p = @(t) _p(t, [100 200 400], 25, .1);
+subplot(2, 2, 3);
+fplot(p, ti);
+title('Plot 3: L / (1 + 25*e^{-.1*t}) L = [100 200 400]');
+
+%% Problem 3
+
+% Copied in each section because unsure if each section run with clean slate
+e = exp(1); 
+
+fprintf('Problem 3 Cell Array')
+r = rand(5); % Stored for later use in cells
+cells = {r, pi^10, r^2;
+	 'Hilbert', @(x) sin(e^x), true};
+disp(cells)
